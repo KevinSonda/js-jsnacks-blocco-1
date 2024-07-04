20714e3f4de6d8fe81350f23d31a1785a57aa357
KevinSonda <kevinsonda@gmail.com>
2024-07-04 22:04:45 +0200

    [UPD]Setto la variabile , recupero pulsante dal dom e valore inserito nel input

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+// definizione dell'array di invitati
+let guest = ['Bread Pitt', 'Kevin Sonda', 'Messi']
+
+// setto la variabile di controllo
+let check_guest = false
+
+//recupero il pulsante dal dom e gli dico di restare in attesa dell'click
+document.getElementById('check-guest').addEventListener('click', function (){
+    //recupero il valore inserito nel campo input
+    let name = document.getElementById('name').value;
+    
+})

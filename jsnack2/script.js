29c628777b5df7455e3e2ce377c1648fca2007b7
KevinSonda <kevinsonda@gmail.com>
2024-07-07 23:06:57 +0200

    [UPD]Verifico se il nunero e dispari e inserisco il numero nell'array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,5 +3,12 @@ let numbers = [];
 
 //eseguo il ciclo di 6 iterazioni 
 for (let i = 1; i <= 6; i++) {
-    
+    // permetto all'utente di inserire il numero
+    let num = prompt('Inserisci un numero');
+
+    //verifico se il numero e dispari
+    if (num % 2 != 0) {
+        //inserisco il nuemro nell'array
+        numbers.push(num)
+    }
 }

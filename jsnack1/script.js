cfd93aa771ec008c6f0966f38c798516c9efda00
KevinSonda <kevinsonda@gmail.com>
2024-07-07 22:55:00 +0200

    [UPD]Ciclo l'array e verifico valore della variabile , fine snack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,5 +8,21 @@ let check_guest = false
 document.getElementById('check-guest').addEventListener('click', function (){
     //recupero il valore inserito nel campo input
     let name = document.getElementById('name').value;
+
+    //ciclo l'array
+    for (let i = 0; i < guest.length; i++){
+        //controllo il valore inserito dall'utente e lo porto in lowercase
+        if (guest[i].toLowerCase() === name.toLowerCase()){
+            check_guest = true
+        }
+    }
     
+    //verifico il valore della variabile di controllo
+    
+    if (check_guest) {
+        console.log(`Benvenuto ${name}, si accomodi`)
+    }
+    else {
+        console.log('ALT! Chi siete , dove andate?!')
+    }
 })
